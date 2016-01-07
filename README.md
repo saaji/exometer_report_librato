34e7a5952a3a640916a9274af73a4237b444c8e4
Adam Lindberg <hello@alind.io>
2016-01-07 15:04:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Librato Reporter for Exometer
+
+An [Exometer][1] reporter for [Librato][2].
+
+## Build
+
+This project uses [Rebar3][3]. It is assumed you have `rebar3` on your path or
+that you are using a Rebar 3 compatible build system.
+
+To compile the code stand alone, run:
+
+```
+$ rebar3 compile
+```
+
+## Dependency
+
+To add the reporter as a dependency, use the following dependency in your
+`rebar.config`:
+
+```erl
+{exometer_report_librato, {git, "https://github.com/eproxus/exometer_report_librato.git"}}
+```
+
+If you have an Elixir project, use the following dependency in your `mix.exs`:
+
+```elixir
+{:exometer_report_librato, github: "eproxus/exometer_report_librato"}
+```
+
+## Configure
+
+The reporter can be configured with the following options:
+
+* `endpoint` *(string)*
+
+    The HTTP endpoint to talk to.
+
+    *Default:* `https://metrics-api.librato.com/v1/metrics`
+
+* `user` *(string)* **(required)**
+
+    The user name used for Librato authentication.
+
+* `token` *(string)* **(required)**
+
+    The API token used for Librato authentication.
+
+* `source` *(string)*
+
+    The source to report to Librato. The default value only uses the node name
+    excluding the host name, because Librato does not support @ characters in a
+    source name.
+
+    *Default:* name part of the Erlang node name
+
+* `interval` *(integer)*
+
+    The interval in milliseconds to send metric batches to Librato.
+
+    *Default:*  `10000`
+
+* `type_map` *(proplist)*
+
+    A type map proplist mapping Exometer metrics to Librato types. If no
+    matching type is found in this map, this reporter defaults to `gauge`.
+
+    Example: `[{[myapp, property], counter}, {[erlang, memory, '_'], gauge}]`
+
+    *Default:* `[]`
+
+[1]: https://github.com/Feuerlabs/exometer_core
+[2]: https://www.librato.com/
+[3]: http://www.rebar3.org/
---
+++ src/exometer_report_librato.erl
@@ -0,0 +1,171 @@
+-module(exometer_report_librato).
+
+-define(ENDPOINT, <<"https://metrics-api.librato.com/v1/metrics">>).
+
+% TODO:
+% [x] Collect metrics until pushed
+% [x] Push at variable timeouts, based on metrics?
+% [x] Validate type map
+
+-compile(export_all).
+
+%--- Callbacks ----------------------------------------------------------------
+-export([exometer_init/1]).
+-export([exometer_setopts/4]).
+-export([exometer_subscribe/5]).
+-export([exometer_report/5]).
+-export([exometer_newentry/2]).
+-export([exometer_info/2]).
+-export([exometer_cast/2]).
+-export([exometer_call/3]).
+-export([exometer_unsubscribe/4]).
+-export([exometer_terminate/2]).
+
+%--- Callbacks ----------------------------------------------------------------
+
+exometer_init(Options) ->
+    Source = fun() -> list_to_binary(string:sub_word(atom_to_list(node()), 1, $@)) end,
+    {ok, #{
+        endpoint => get(endpoint, Options, ?ENDPOINT),
+        user     => get(user, Options),
+        token    => get(token, Options),
+        source   => get(source, Options, Source),
+        interval => get(interval, Options, 10000),
+        type_map => validate_type_map(get(type_map, Options, [])),
+        last     => erlang:monotonic_time(milli_seconds),
+        metrics  => #{
+            counters => [],
+            gauges   => []
+        }
+    }}.
+
+exometer_setopts(_Metric, _Options, _Status, State) ->
+    {ok, State}.
+
+exometer_subscribe(_Metric, _DataPoint, _Interval, _Extra, State) ->
+    {ok, State}.
+
+exometer_report(Metric, DataPoint, Extra, Value, State) ->
+    % TODO: Clean up
+    NewState = add_metric(Metric, DataPoint, Value, Extra, State),
+    case check_timeout(NewState) of
+        {timeout, NewNewState} ->
+            {ok, send_metrics(NewNewState)};
+        {wait, NewState} ->
+            {ok, NewState}
+    end.
+
+exometer_newentry(Entry, State) ->
+    error_logger:info_msg("Unknown entry ~p~n", [Entry]),
+    {ok, State}.
+
+exometer_info(Unknown, State) ->
+    error_logger:info_msg("Unknown info ~p~n", [Unknown]),
+    {ok, State}.
+
+exometer_cast(Unknown, State) ->
+    error_logger:info_msg("Unknown cast ~p~n", [Unknown]),
+    {ok, State}.
+
+exometer_call(Unknown, From, State) ->
+    error_logger:info_msg("Unknown call ~p from ~p~n", [Unknown, From]),
+    {ok, State}.
+
+exometer_unsubscribe(_Metric, _DataPoint, _Extra, State) ->
+    {ok, State}.
+
+exometer_terminate(_Reason, _State) ->
+    ok.
+
+%--- Internal -----------------------------------------------------------------
+
+get(Option, Options) ->
+    get(Option, Options, fun() -> error({missing_option, Option}) end).
+
+get(Option, Options, Default) when is_function(Default) ->
+    case proplists:get_value(Option, Options) of
+        undefined -> Default();
+        Value     -> Value
+    end;
+get(Option, Options, Default) ->
+    get(Option, Options, fun() -> Default end).
+
+validate_type_map(TypeMap) -> [validate_type(T) || T <- TypeMap].
+
+validate_type({_, counter} = Mapping) -> Mapping;
+validate_type({_, gauge} = Mapping)   -> Mapping;
+validate_type({_, spiral} = Mapping)  -> Mapping;
+validate_type({_, Type})              -> error({unsupported_type, Type}).
+
+add_metric(Metric, DataPoint, Value, Extra, State) ->
+    #{metrics := Metrics, type_map := TypeMap} = State,
+    Time = erlang:system_time(seconds),
+    Type = case exometer_util:report_type(Metric, Extra, TypeMap) of
+        {ok, counter} -> counters;
+        {ok, spiral}  -> counters;
+        {ok, gauge}   -> gauges;
+        error         -> gauges
+    end,
+    State#{metrics := Metrics#{
+        Type := [{Metric, DataPoint, Value, Time}|maps:get(Type, Metrics)]
+    }}.
+
+check_timeout(#{interval := Interval, last := Last} = State) ->
+    Current = erlang:monotonic_time(milli_seconds),
+    if
+        Current - Last >= Interval -> {timeout, State#{last := Current}};
+        true                       -> {wait, State}
+    end.
+
+send_metrics(#{metrics := Metrics, source := Source} = State) ->
+    try
+        #{counters := Counters, gauges := Gauges} = Metrics,
+        Payload = jiffy:encode(#{
+            counters => [metric(C, Source) || C <- Counters],
+            gauges => [metric(G, Source) || G <- Gauges]
+        }),
+
+        case post(uri(State), [auth(State)], Payload) of
+            {ok, {{_, 200, _}, _Headers, _Body}} ->
+                ok;
+            {ok, {{_, Status, Reason}, _Headers, _Body}} ->
+                error_logger:error_msg("unexpected response: ~p ~s~n", [Status, Reason]);
+            {error, Reason} ->
+                error_logger:error_msg("unexpected error: ~p~n", [Reason])
+        end
+    catch
+        E:R -> error_logger:error_msg("unexpected error ~p ~p~n", [E, R])
+    end,
+    State#{metrics := #{counters => [], gauges => []}}.
+
+uri(#{endpoint := Endpoint}) -> binary_to_list(Endpoint).
+
+post(URL, Headers, Body) ->
+    httpc:request(post, {URL, Headers, "application/json", Body}, [{timeout, 5000}], [{body_format, binary}]).
+
+metric({Metric, DataPoint, Value, Time}, Source) ->
+    #{
+        name => name(Metric ++ [DataPoint]),
+        value => Value,
+        measure_time => Time,
+        source => Source
+    }.
+
+% [a, b] -> "a.b"
+% [a, b, value] -> "a.b"
+% [] -> "" | error
+% [a, b]
+
+name([Key|Metric]) ->
+    iolist_to_binary([atom_to_binary(Key, utf8), names(Metric)]).
+
+names([]) ->
+    [];
+names([value]) ->
+    [];
+names([Key|Metric]) ->
+    [$., atom_to_binary(Key, utf8)|names(Metric)].
+
+auth(#{user := User, token := Token}) ->
+    Hash = base64:encode_to_string(lists:append([User, ":", Token])),
+    {"Authorization", "Basic " ++ Hash}.
